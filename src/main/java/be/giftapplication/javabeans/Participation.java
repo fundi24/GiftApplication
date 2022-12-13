c69edefef9f8002e43fe806a3443e05e0612af1c
Fundi POSHI <fundi.poshi@condorcet.be>
2022-12-13 15:19:45 +0100

    Creation des classes DAO + correction nommage table "Participation"

--- src/main/java/be/giftapplication/javabeans/Participation.java
+++ src/main/java/be/giftapplication/javabeans/Participation.java
@@ -2,24 +2,24 @@ package be.giftapplication.javabeans;
 
 import java.io.Serializable;
 
-public class History implements Serializable {
+public class Participation implements Serializable {
 	private static final long serialVersionUID = -4098840781809412627L;
-	private int idHistory;
+	private int idParticipation;
 	private double amountPaid;
 	private Customer customer;
 	private Gift gift;
 	
-	public History()
+	public Participation()
 	{
 		
 	}
 
-	public int getIdHistory() {
-		return idHistory;
+	public int getIdParticipation() {
+		return idParticipation;
 	}
 
-	public void setIdHistory(int idHistory) {
-		this.idHistory = idHistory;
+	public void setIdParticipation(int idParticipation) {
+		this.idParticipation = idParticipation;
 	}
 
 	public double getAmountPaid() {
