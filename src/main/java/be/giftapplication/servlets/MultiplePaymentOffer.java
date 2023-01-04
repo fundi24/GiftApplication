4d4287334f325ee49f53111450d1e1131515acfd
LaeticiaKH <laeticia.khoulalene@condorcet.be>
2023-01-04 19:44:16 +0100

    Fin fonctionnalité Offrir + début fonctionnalité Offrir à plusieurs

---
+++ src/main/java/be/giftapplication/servlets/MultiplePaymentOffer.java
@@ -0,0 +1,62 @@
+package be.giftapplication.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import be.giftapplication.javabeans.Gift;
+import be.giftapplication.javabeans.ListGift;
+
+/**
+ * Servlet implementation class MultiplePaymentOffer
+ */
+
+public class MultiplePaymentOffer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MultiplePaymentOffer() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			HttpSession session = request.getSession();
+			ListGift listgift = (ListGift) session.getAttribute("listgift");
+			int idGift = Integer.parseInt(request.getParameter("idGift"));
+
+			Gift gift = listgift.getGifts().stream().filter(l -> l.getIdGift() == idGift).findFirst().orElse(null);
+
+			request.setAttribute("gift", gift);
+
+			getServletContext().getRequestDispatcher("/WEB-INF/MultiplePaymentOffer.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			getServletContext().getRequestDispatcher("/WEB-INF/ErrorPage.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+	}
+
+}
